210182e9798166d5dfc771453ff9eea29283617f
矢野美羽 <yanomihane@yanomiwanoMacBook-Pro.local>
2023-04-08 17:38:34 +0900

    [add]code

---
+++ alert.js
@@ -0,0 +1,16 @@
+function loadHello(){
+    alert("Hello!");
+  }
+  function sum(){
+    let x = 3;
+    let y = 4;
+    let z = x + y;
+    alert(z);
+  }
+  function sum(){
+    let x = 3;      // ← xという箱を作り、その中に3を入れろ！という命令
+    let y = 4;      // ← yという箱を作り、その中に4を入れろ！という命令
+    let z = x + y;  // ← zという箱を作り、その中にxの中身（3）とyの中身（4）を足した値を入れろ！という命令
+    alert(z);       // ← zの中身をアラートで表示しろ！という命令
+  }
+  
